84f0f0835a31a1a3f43ebc0bd2fedde63b09238b
QueenLeonidas <leoni.schulte@googlemail.com>
2018-01-31 14:42:56 +0100

    Fertistellung

---
+++ Assets/Models/SceneSwitch.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    public void GotoStartScene()
+    {
+        SceneManager.LoadScene("StartZustand");
+    }
+
+    public void GotoEndScene()
+    {
+        SceneManager.LoadScene("EndZustand");
+    }
+
+    public void GotoMenuScene()
+    {
+
+        SceneManager.LoadScene("Menu");
+
+    }
+
+    void Update()
+
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GotoMenuScene();
+        }
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+
+
+    }
+}
---
+++ Assets/SplineTweener.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Pixelplacement;
+
+[RequireComponent(typeof(Spline))]
+public class SplineTweener : MonoBehaviour
+{
+    [SerializeField] Transform myObject;
+    [SerializeField] bool faceDirection = true;
+    [SerializeField] float duration = 2f;
+    [SerializeField] AnimationCurve animationCurve = AnimationCurve.Linear(0,0,1,1);
+    [SerializeField] Tween.LoopType loopType = Tween.LoopType.Loop;
+
+    private Spline mySpline;
+
+    void Start()
+    {
+        mySpline = GetComponent<Spline>();
+
+        Tween.Spline(mySpline, myObject, 0, 1, faceDirection, duration, 0, animationCurve, loopType);
+    }
+}
